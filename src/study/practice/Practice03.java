2f7cbf3179f7643a52e4b9345affbb5677ff9b88
JEONGMIN0000 <leewjdals122900@gmail.com>
2024-10-18 10:22:38 +0900

    변수 & 연산자 연습

--- src/study/practice/Practice03.java
+++ src/study/practice/Practice03.java
@@ -1,61 +1,21 @@
-//package study.practice;
+package study.practice;
+
+public class Practice03 {
+
+	public static void main(String[] args) {
+		
+//		3. 아래 조건 범위에 맞춰서 결과가 나올 수 있도록, 삼항연산자 코드를 완성하세요.
 //
-//public class Practice03 {
-//
-//	public static void main(String[] args) {
-//		
-//		int x = 10;
-//		int y = 4;
-//
-//		//1) 결과가 2가 나오도록 작성하세요
-//		int result = x/y;
-//		System.out.println(result);
-//		//2) 결과가 2.5가 나오도록 작성하세요
-//		double result = (double)x/y;
-//		System.out.println(result);
-//	
-//		//2. 결과가 7.5가 나오도록 작성하세요 
-//		double a = 3.5;
-//		double b = 4.7;
-//		//double result = _______________;
-//		
-//		double result = a+(int)b;
-//		System.out.println(result);
-//	
-//		//3. 계산결과가 12가 나오도록 작성하세요
-//		String a = "3.4";
-//		String b = "4";
-//		int result = (int)Double.parseDouble(a) * Integer.parseInt(b);
-//		
-//		System.out.println(result);
-//	
-//	
-//		//4. 계산결과가 "1013" 이 나오도록 코드를 작성하세요.
-//		String a = "10";
-//		int b = 3;
-//		double c = 4.5;
-//		String result = a + (int)(b * c);
-//		System.out.println(result);
-//	
-//	
-//		//5. 계산결과가 "243" 이 나오도록 코드를 작성하세요. ------------------
-//		int a = 4;
-//		double b = 3.4;
-//		String c = "6.8";
-//		String result = a*(int)Double.parseDouble(c)+ String.valueOf((int)b);
-//		System.out.println(result);
-//	
-//		//6. 계산 결과가 아래와 같이 나오도록 코드를 작성하세요.
-//		int x = 111;
-//		int y = 13;
-//	
-//		//1) 출력결과 : 7
-//		int result = x%y;
-//		System.out.println(result);
-//		//2) 출력결과 : 8
-//		int result = x/y;
-//		System.out.println(result);
-//
-//	}
-//
-//}
+//		점심 메뉴 가격에 따라서 아래 결과를 출력하고 싶습니다.
+//		6000원 이하 -> "훌륭"
+//		6000원 초과 ~ 8000원 -> "선방"
+//		8000원 초과 ~ 12000원 -> "눈물"
+//		12000원 초과 -> "물로배채우기"
+
+		int menuPrice = 8000; //점심 식사 메뉴 가격 (임의로 변경해가면서 테스트)
+		String result = (menuPrice>6000) ? (menuPrice>8000 ? (menuPrice>12000? "물로 배채우기":"눈물") : "선방") : "훌륭";
+		System.out.println(result);
+
+	}
+
+}

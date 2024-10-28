79da9774e6d778c703c2cfb1a570f4707d9a94a0
JEONGMIN0000 <leewjdals122900@gmail.com>
2024-10-28 17:03:14 +0900

    클래스 필드/생성자/메소드

---
+++ src/study/cls/cls02/Phone.java
@@ -0,0 +1,40 @@
+package study.cls.cls02;
+
+public class Phone {
+	
+	//필드
+	String model; 	//기본값X, 무조건 필요
+	int price; 		//기본값 : 100
+	int battery; 	//기본값 : 3000
+	
+	//기본생성자
+	Phone() {
+		
+	}
+	
+	Phone(String model) {
+		this(model,100,3000);
+		//this.model = model;
+	}
+	
+	Phone(String model, int price){
+		this(model,price,3000);
+		//this.model = model;
+		//this.price = price;
+		//this.battery = 3000;
+	}
+	
+	Phone(String model, int price, int battery){
+		this.model = model;
+		this.price = price;
+		this.battery = battery;
+	}
+	
+	//재부팅
+	//사용자등록
+	//필름부착
+	//알콜소독
+	
+	
+	
+}
---
+++ src/study/cls/cls02/PhoneMain.java
@@ -0,0 +1,11 @@
+package study.cls.cls02;
+
+public class PhoneMain {
+	
+	Phone p1 = new Phone("갤럭시"); //생성자 기본생성자
+	Phone p2 = new Phone("아이폰");
+	//Phone p3 = new Phone();
+	Phone p3 = new Phone("갤럭시", 300, 4000);
+	Phone p4 = new Phone("갤럭시", 300);
+	
+}

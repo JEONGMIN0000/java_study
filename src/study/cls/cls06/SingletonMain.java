277f1059f4d08b6901ddcc1b2e0872fe5deafd03
JEONGMIN0000 <leewjdals122900@gmail.com>
2024-12-17 16:52:55 +0900

    싱글톤 패턴

---
+++ src/study/cls/cls06/SingletonMain.java
@@ -0,0 +1,17 @@
+package study.cls.cls06;
+
+public class SingletonMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//외부생성 불가능
+		//Singleton s1 = new Singleton(); 
+		//Singleton s2 = new Singleton(); 
+		
+		Singleton st = Singleton.getInstance();
+		
+
+	}
+
+}

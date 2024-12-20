3aeb5eeb94fafee2946c457674afd473bce412b7
JEONGMIN0000 <leewjdals122900@gmail.com>
2024-12-20 16:27:34 +0900

    클래스 상속 타입변환 & 다형성

---
+++ src/study/inherit/inherit04/v2/Cleaner.java
@@ -0,0 +1,22 @@
+package study.inherit.inherit04.v2;
+
+public class Cleaner {
+	
+//	public void clean() {
+//		System.out.println("나는 클리너");
+//	}
+	
+	public void removeDust() {
+		System.out.println("Cleaner 먼지털기");
+	}
+
+	
+	public void cleanFloor() {
+		System.out.println("Cleaner 바닥쓸기");
+	}
+	
+	public void washWindow() {
+		System.out.println("Cleaner 창문닦기");
+	}
+
+}

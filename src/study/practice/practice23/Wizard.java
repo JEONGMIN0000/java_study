5f22cba687882e78992540c35f52791ea032f651
JEONGMIN0000 <leewjdals122900@gmail.com>
2024-12-20 16:29:17 +0900

    클래스 상속, 다형성 연습문제

---
+++ src/study/practice/practice23/Wizard.java
@@ -0,0 +1,13 @@
+package study.practice.practice23;
+
+public class Wizard extends Novice{
+	
+	/* 2. mp 필드를 추가하세요. */
+	int mp;
+
+	/* 3. fireball() 메소드를 만드세요. */
+	public void fireball() {
+		System.out.printf("[%s]의 파이어볼@@\n", name);
+	}
+
+}

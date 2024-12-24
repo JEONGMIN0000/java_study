a0e41128003a6465616fadecde8ea66986c026c1
JEONGMIN0000 <leewjdals122900@gmail.com>
2024-12-24 17:27:40 +0900

    클래스 상속 형변환

---
+++ src/study/inherit/inherit04/v4/DustCleaner.java
@@ -0,0 +1,13 @@
+package study.inherit.inherit04.v4;
+
+public class DustCleaner extends Cleaner {
+	
+	public void doClean() {
+		System.out.println("먼지털기");
+	}
+	
+	public void arrangeItem() {
+		System.out.println("물건정리");
+	}
+
+}
---
+++ src/study/inherit/inherit04/v4/FloorCleaner.java
@@ -0,0 +1,9 @@
+package study.inherit.inherit04.v4;
+
+public class FloorCleaner extends Cleaner {
+	
+	public void doClean() {
+		System.out.println("바닥쓸기");
+	}
+	
+}
